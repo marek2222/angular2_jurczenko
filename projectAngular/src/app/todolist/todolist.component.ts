191b48b820e8642bfe9b95086bc6a07b817d66a8
marek2222 <marekmusial2@o2.pl>
2019-05-06 08:59:50 +0200

    1.2: Tworzenie listy zadań todolist:
      Utworzenie nowego komponentu:
        $ ng generate component todolist
      lub
        $ ng g        component todolist
      Komponent zagnieżdzam w app.component.html:
        <app-todolist>Loading...</app-todolist>

---
+++ projectAngular/src/app/todolist/todolist.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.component.html',
+  styleUrls: ['./todolist.component.css']
+})
+export class TodolistComponent {
+    title = 'lista rzeczy do zrobienia';
+}
